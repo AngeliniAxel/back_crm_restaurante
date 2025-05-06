4885d8d2c61ef5e5a8d914aba04449c3c73c4de9
Juanita Escobar <juanitaescobar85@gmail.com>
2025-05-06 02:49:42 -0500

    confi-users-routes

--- src/routes/api.routes.js
+++ src/routes/api.routes.js
@@ -2,4 +2,6 @@ const router = require("express").Router();
 
 router.use("/menus", require("./api/menus.routes"));
 
+router.use("/users", require("./api/users.routes"));
+
 module.exports = router;
