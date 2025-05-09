e279beeb326e199baa579f5bcb7ed89df3cae96f
Juanita Escobar <juanitaescobar85@gmail.com>
2025-05-09 05:42:37 -0500

    add checktoken reservations.routes

--- src/routes/api.routes.js
+++ src/routes/api.routes.js
@@ -1,3 +1,6 @@
+const { checkTableId } = require('../middlewares/tables.middleware');
+const { checkToken } = require('../middlewares/users.middleware');
+
 const router = require('express').Router();
 
 router.use('/menus', require('./api/menus.routes'));
@@ -8,6 +11,6 @@ router.use('/reviews', require('./api/reviews.routes'));
 
 router.use('/tables', require('./api/tables.routes'));
 
-router.use('/reservations', require('./api/reservations.routes'));
+router.use('/reservations', checkToken, require('./api/reservations.routes'));
 
 module.exports = router;
